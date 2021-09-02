287e8c1ed23823ea8bf09cfd99cf22253c063e42
paddyroddy <patrickjamesroddy@gmail.com>
2021-09-02 12:41:00 +0100

    Make separate CLI utils file

--- denoising_demo/scripts/denoise_earth.py
+++ denoising_demo/scripts/denoise_earth.py
@@ -2,34 +2,31 @@ import pyssht as ssht
 
 from denoising_demo.data.create_earth_flm import create_flm
 from denoising_demo.plotting.create_plot_sphere import Plot
+from denoising_demo.utils.cli import read_args
 from denoising_demo.utils.denoising import perform_denoising
 from denoising_demo.utils.noise import compute_snr, create_noise
 from denoising_demo.utils.wavelet_methods import create_axisymmetric_wavelets
 
-B = 2
-J_MIN = 0
-L = 128
-N_SIGMA = 3
-SMOOTHING = 2
-SNR_IN = 10
-
 
 def main() -> None:
     """Performs a denoising akin to figure 5 of the S2LET paper"""
+    # read in command line arguments
+    args = read_args()
+
     # create initial smoothed Earth topography
-    earth_flm = create_flm(L, smoothing=SMOOTHING)
+    earth_flm = create_flm(args.bandlimit, smoothing=args.smoothing)
 
     # compute harmonic coefficients of the noise to create noised signal
-    nlm = create_noise(L, earth_flm, SNR_IN)
+    nlm = create_noise(args.bandlimit, earth_flm, args.noise)
     compute_snr(earth_flm, nlm)
     noised_earth_flm = earth_flm + nlm
 
     # create axisymmetric wavelets for hard-thresholding
-    wavelets = create_axisymmetric_wavelets(L, B, J_MIN)
+    wavelets = create_axisymmetric_wavelets(args.bandlimit, args.parameter, args.jmin)
 
     # denoise Earth signal
     denoised_earth_flm = perform_denoising(
-        L, earth_flm, noised_earth_flm, wavelets, SNR_IN, N_SIGMA
+        args.bandlimit, earth_flm, noised_earth_flm, wavelets, args.noise, args.sigma
     )
 
     # create dict to loop over
@@ -42,10 +39,10 @@ def main() -> None:
     # produce three plots
     for name, flm in fields_dict.items():
         # convert to pixel space
-        field = ssht.inverse(flm, L)
+        field = ssht.inverse(flm, args.bandlimit)
 
         # perform plot
-        Plot(field, L, name).execute()
+        Plot(field, args.bandlimit, name).execute()
 
 
 if __name__ == "__main__":
---
+++ denoising_demo/utils/cli.py
@@ -0,0 +1,67 @@
+from argparse import ArgumentParser, Namespace
+
+from denoising_demo.utils.vars import (
+    B_DEFAULT,
+    J_MIN_DEFAULT,
+    L_DEFAULT,
+    N_SIGMA_DEFAULT,
+    SMOOTHING_DEFAULT,
+    SNR_IN_DEFAULT,
+)
+
+
+def read_args() -> Namespace:
+    """Method to read arguments from the command line
+
+    Returns:
+        Namespace: a argparse Namespace object
+    """
+    parser = ArgumentParser(description="Create denoising plots")
+    parser.add_argument(
+        "--bandlimit", "-L", type=int, default=L_DEFAULT, help="bandlimit"
+    )
+    parser.add_argument(
+        "--jmin",
+        "-j",
+        type=int,
+        default=J_MIN_DEFAULT,
+        help="the minimum wavelet scale",
+    )
+    parser.add_argument(
+        "--parameter",
+        "-B",
+        type=int,
+        default=B_DEFAULT,
+        help="the positive real parameter",
+    )
+    parser.add_argument(
+        "--noise",
+        "-n",
+        type=int,
+        default=SNR_IN_DEFAULT,
+        help="the SNR_IN of the noise level",
+    )
+    parser.add_argument(
+        "--sigma",
+        "-si",
+        type=int,
+        default=N_SIGMA_DEFAULT,
+    )
+    parser.add_argument(
+        "--smoothing",
+        "-sm",
+        type=int,
+        default=SMOOTHING_DEFAULT,
+        help="the scaling of the sigma in Gaussian smoothing of the Earth",
+    )
+    parser.add_argument(
+        "--type",
+        "-t",
+        type=str,
+        nargs="?",
+        default="real",
+        const="real",
+        choices=["abs", "real", "imag", "sum"],
+        help="plotting type: defaults to real",
+    )
+    return parser.parse_args()
